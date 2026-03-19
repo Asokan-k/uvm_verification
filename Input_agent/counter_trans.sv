04331ec164d7afc5970b8149d2254f6d88d44fdb
Asokan k <asokansrm03@gmail.com>
2026-03-19 18:16:18 +0530

    Add files via upload

---
+++ Input_agent/counter_trans.sv
@@ -0,0 +1,47 @@
+class counter_trans extends uvm_sequence_item;
+  `uvm_object_utils(counter_trans)
+
+  rand logic rst;
+  rand logic load;
+  rand logic [3:0] data_in;
+       logic [3:0] data_out;
+
+  static int no_of_xtn;
+
+  constraint VALID_RST  { rst  dist {1 := 1, 0 := 15}; }
+  constraint VALID_LOAD { load dist {1 := 1, 0 := 15}; }
+  constraint VALID_DATA { data_in inside {[0:15]}; }
+
+  function new(string name = "counter_trans");
+    super.new(name);
+  endfunction
+
+  function bit do_compare(uvm_object rhs, uvm_comparer comparer);
+    counter_trans rhs_;
+
+    if (!$cast(rhs_, rhs)) begin
+      `uvm_fatal("do_compare", "cast of the rhs object failed")
+      return 0;
+    end
+
+    return super.do_compare(rhs, comparer) &&
+           (data_out == rhs_.data_out);
+  endfunction
+
+  function void do_print(uvm_printer printer);
+    super.do_print(printer);
+
+    printer.print_field("rst",      this.rst,      1, UVM_DEC);
+    printer.print_field("load",     this.load,     1, UVM_DEC);
+    printer.print_field("data_in",  this.data_in,  4, UVM_DEC);
+    printer.print_field("data_out", this.data_out, 4, UVM_DEC);
+  endfunction
+
+  function void post_randomize();
+    no_of_xtn++;
+    `uvm_info("randomized data",
+      $sformatf("randomized transaction \n%s",
+      this.sprint()), UVM_MEDIUM)
+  endfunction
+
+endclass
